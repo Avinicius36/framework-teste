a70441b322262a319f46b754c911bd41d570e5bb
@anvinicius <antonio.araujo@engdb.com.br>
2020-09-30 22:44:42 -0300

    Comittando os projetos

---
+++ blog-application/coddify-blog/coddify-core/src/main/java/coddify/blog/configuration/BeanConfiguration.java
@@ -0,0 +1,18 @@
+package coddify.blog.configuration;
+
+import coddify.blog.repository.BlogRepository;
+import coddify.blog.service.BlogService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+public class BeanConfiguration {
+
+    @Bean
+    public BlogService blogService(BlogRepository blogRepository) {
+        return new BlogService(blogRepository);
+    }
+}
